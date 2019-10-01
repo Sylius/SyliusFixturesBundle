1ed143a9ef2ccfd605b9988292bf25a073d466c3
mamazu <14860264+mamazu@users.noreply.github.com>
2019-10-01 10:53:25 +0200

    Adding configuratiion test

---
+++ tests/Listener/SuiteLoaderListenerTest.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+namespace Sylius\Bundle\FixturesBundle\Tests\Listener;
+
+use Matthias\SymfonyConfigTest\PhpUnit\ConfigurationTestCaseTrait;
+use PHPUnit\Framework\TestCase;
+use Sylius\Bundle\FixturesBundle\Listener\SuiteLoaderListener;
+use Sylius\Bundle\FixturesBundle\Loader\SuiteLoaderInterface;
+use Sylius\Bundle\FixturesBundle\Suite\SuiteRegistryInterface;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+final class SuiteLoaderListenerTest extends TestCase
+{
+    use ConfigurationTestCaseTrait;
+
+    /**
+     * @test
+     */
+    public function by_default_it_has_an_empty_suite_list(): void
+    {
+        $this->assertProcessedConfigurationEquals([[]], ['suites' => []], 'suites');
+    }
+
+    public function it_processes_the_suites(): void
+    {
+        $this->assertProcessedConfigurationEquals([['suites' => ['abc']]], [['suites' => ['abc']]], 'suites');
+    }
+
+    protected function getConfiguration(): ConfigurationInterface
+    {
+        return new SuiteLoaderListener(
+            $this->getMockBuilder(SuiteRegistryInterface::class)->getMock(),
+            $this->getMockBuilder(SuiteLoaderInterface::class)->getMock()
+        );
+    }
+}
