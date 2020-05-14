8a423a6071c4ca1cce5085a1397306b6039e8ba0
mamazu <maxo.pech@gmail.com>
2020-05-14 20:04:24 +0200

    Enabling phpunit tests

--- tests/Loader/FixtureListCommandTest.php
+++ tests/Loader/FixtureListCommandTest.php
@@ -3,7 +3,10 @@ declare(strict_types=1);
 
 namespace Sylius\Bundle\FixturesBundle\Tests\Loader;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Sylius\Bundle\FixturesBundle\Command\FixturesLoadCommand;
+use Sylius\Bundle\FixturesBundle\Fixture\FixtureRegistry;
+use Sylius\Bundle\FixturesBundle\Suite\LazySuiteRegistry;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Console\Tester\CommandTester;
@@ -13,19 +16,24 @@ final class FixtureListCommandTest extends KernelTestCase
     /** @var CommandTester */
     private $commandTester;
 
+    protected static $container;
+
     protected function setUp()
     {
         self::$kernel = static::createKernel();
         self::$kernel->boot();
         self::$container = self::$kernel->getContainer();
 
-        self::$container
-            ->get('sylius_fixtures.fixture_registry')
-            ->addFixture(new SampleFixture(self::$container->get('doctrine.orm.default_entity_manager')))
-        ;
+        /** @var FixtureRegistry $fixtureRegistry */
+        $fixtureRegistry = self::$container->get('sylius_fixtures.fixture_registry');
 
-        self::$container
-            ->get('sylius_fixtures.suite_registry')
+        /** @var EntityManagerInterface $entityManager */
+        $entityManager = self::$container->get('doctrine.orm.default_entity_manager');
+        $fixtureRegistry->addFixture(new SampleFixture($entityManager));
+
+        /** @var LazySuiteRegistry $suiteRegistry */
+        $suiteRegistry = self::$container->get('sylius_fixtures.suite_registry');
+        $suiteRegistry
             ->addSuite('default', [
                 'fixtures'  => $this->createFixture('sample_fixture'),
                 'listeners' => [],
--- tests/Loader/FixtureLoaderTest.php
+++ tests/Loader/FixtureLoaderTest.php
@@ -4,11 +4,17 @@ declare(strict_types=1);
 namespace Sylius\Bundle\FixturesBundle\Tests\Loader;
 
 use Doctrine\ORM\EntityManagerInterface;
+use PHPUnit\Framework\MockObject\MockObject;
+use Psr\Log\LoggerInterface;
 use Sylius\Bundle\FixturesBundle\Command\FixturesLoadCommand;
+use Sylius\Bundle\FixturesBundle\Fixture\FixtureRegistry;
+use Sylius\Bundle\FixturesBundle\Fixture\FixtureRegistryInterface;
+use Sylius\Bundle\FixturesBundle\Suite\LazySuiteRegistry;
 use Sylius\Bundle\FixturesBundle\Suite\SuiteNotFoundException;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Console\Tester\CommandTester;
+use Webmozart\Assert\Assert;
 
 final class FixtureLoaderTest extends KernelTestCase
 {
@@ -18,22 +24,40 @@ final class FixtureLoaderTest extends KernelTestCase
     /** @var EntityManagerInterface */
     private $em;
 
+    /** @var MockObject|LoggerInterface */
+    private $logger;
+
+    protected static $container;
+
+    public function getLazySuiteRegistry(): LazySuiteRegistry
+    {
+        /** @var LazySuiteRegistry $lazySuiteRegistry */
+        $lazySuiteRegistry = self::$container->get('sylius_fixtures.suite_registry');
+
+        return $lazySuiteRegistry;
+    }
+
     protected function setUp()
     {
         self::$kernel = static::createKernel();
         self::$kernel->boot();
         self::$container = self::$kernel->getContainer();
 
-        $this->em = self::$container->get('doctrine.orm.default_entity_manager');
+        $this->logger = $this->createMock(LoggerInterface::class);
+        self::$container->set('sylius_fixtures.logger', $this->logger);
+
+        /** @var EntityManagerInterface $entityManager */
+        $entityManager = self::$container->get('doctrine.orm.default_entity_manager');
+        $this->em = $entityManager;
         $connection = $this->em->getConnection();
 
         $connection->exec('CREATE TABLE IF NOT EXISTS testTable (test_column varchar(255) NOT NULL);');
         $connection->exec('DELETE FROM testTable');
 
-        self::$container
-            ->get('sylius_fixtures.fixture_registry')
-            ->addFixture(new SampleFixture($this->em))
-        ;
+        /** @var FixtureRegistry $registry */
+        $registry = self::$container->get('sylius_fixtures.fixture_registry');
+        Assert::isInstanceOf($registry, FixtureRegistryInterface::class);
+        $registry->addFixture(new SampleFixture($this->em));
 
         $application = new Application(self::$kernel);
         $application->add(new FixturesLoadCommand());
@@ -41,7 +65,7 @@ final class FixtureLoaderTest extends KernelTestCase
         $this->commandTester = new CommandTester($command);
     }
 
-    private function createFixture(string $name, array $options = []): array
+    private function createConfiguration(string $name, array $options = []): array
     {
         return [
             $name => [
@@ -56,10 +80,9 @@ final class FixtureLoaderTest extends KernelTestCase
      */
     public function loads_default_suite_if_none_is_defined(): void
     {
-        self::$container
-            ->get('sylius_fixtures.suite_registry')
+        $this->getLazySuiteRegistry()
             ->addSuite('default', [
-                'fixtures'  => $this->createFixture('sample_fixture'),
+                'fixtures'  => $this->createConfiguration('sample_fixture'),
                 'listeners' => [],
             ])
         ;
@@ -68,7 +91,26 @@ final class FixtureLoaderTest extends KernelTestCase
 
         $connection = $this->em->getConnection();
         $result = $connection->fetchArray('SELECT count(*) FROM testTable WHERE test_column = "test";');
-        $this->assertSame(1, (int) $result[0]);
+        $this->assertSame(1, (int)$result[0]);
+    }
+
+    /**
+     * @test
+     */
+    public function it_loads_the_specified_suite(): void
+    {
+        $this->getLazySuiteRegistry()
+            ->addSuite('sample', [
+                'fixtures'  => $this->createConfiguration('sample_fixture'),
+                'listeners' => [],
+            ])
+        ;
+
+        $this->commandTester->execute(['suite' => 'sample'], ['interactive' => false]);
+
+        $connection = $this->em->getConnection();
+        $result = $connection->fetchArray('SELECT count(*) FROM testTable WHERE test_column = "test";');
+        $this->assertSame(1, (int)$result[0]);
     }
 
     /**
