8dfb097a0a9df9df4bb08bf804739ed1e9b82457
Ivan I. Atanasov <i.atanasov77@gmail.com>
2026-06-27 09:07:21 +0000

    The SymfonyComponentHttpKernelDependencyInjectionExtension class is considered internal since Symfony 7.1, to be deprecated in 8.1

--- src/DependencyInjection/SyliusFixturesExtension.php
+++ src/DependencyInjection/SyliusFixturesExtension.php
@@ -22,7 +22,7 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 
 final class SyliusFixturesExtension extends Extension implements PrependExtensionInterface
 {
